739aa6519740bd6059f3f604a2714e9016f79cf4
Ceasar Cyrillus <ceasarcyrillus@businesspartner.roche.com>
2021-02-20 22:32:29 +0100

    Added icons to the header

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,16 +2,32 @@ import React from "react";
 import "./Header.css"
 
 export const Header = () => {
-  const today = new Date().toLocaleDateString()
+  const now = new Date();
+  const currentWeekDay = weekDays[now.getDay()];
+  const currentDate = now.toLocaleDateString();
   return(
     <div className={"header-wrapper"}>
       <header className={"primary-header"}>
-        <h1 className={"header-text"}>Fun Fact</h1>
+        <h1 className={"header-text"}>
+          Random Fact
+          <i className="fas fa-random header-icon"/>
+        </h1>
       </header>
       <header className={"secondary-header"}>
-        <h3 className={"header-text"}>{today}</h3>
+        <h3 className={"header-text"}>{`${currentWeekDay} ${currentDate}`}
+          <i className="far fa-calendar-alt header-icon"/>
+        </h3>
       </header>
     </div>
     )
 
 }
+const weekDays = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday"
+];
