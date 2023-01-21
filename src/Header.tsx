daa925d915efff8e84adc17f863075a79b31ecc9
ceasarcyrillus <ceasar.cyrillus@protonmail.com>
2023-01-21 13:12:44 +0100

    Add for sale banner

--- src/Header.tsx
+++ src/Header.tsx
@@ -10,5 +10,10 @@ export const Header = () =>
           <i className="fas fa-random header-icon"/>
         </h1>
       </header>
+      <header className={"secondary-header"}>
+        <h2 className={"secondary-header-text"}>
+          The domain <b><a href={"/"}>randomfunfact.com</a></b> is for sale. Send an email for more information: <a href={"mailTo:ceasar.cyrillus@protonmail.com"}>ceasar.cyrillus@protonmail.com</a>
+        </h2>
+      </header>
     </div>
   )
